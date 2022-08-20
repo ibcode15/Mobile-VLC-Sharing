cd72e9f594c8c3589bc3b6264a3f936f1314d548
Isaac Butchard <59832706+ibcode15@users.noreply.github.com>
2022-08-20 11:53:50 +0100

    Moved out upload function to another file etc
    Changed some of the logging functions due to changes to the logging class.
    Moved the uploading function to its own file for tidiness.
    Added loadingbar argument, if you type "--noloadingbar", it will change loadingbar's value to False and result in a different loading screen without a bar.

--- MediaUploader.py
+++ MediaUploader.py
@@ -1,51 +1,26 @@
-import os
-import sys
 import fire
-import requests
 import src.File as File
 import src.Utilities as Utils
+import src.Uploader as Uploader
 
-
-
-def Upload(VLC_IP: str,file: File.File,Log: Utils.Logging | None = None) -> int:
+def Main(VLC_IP: str,*MediaFiles: tuple[str],loadingbar: bool = True) -> None:
     try:
-        req = requests.post(f"http://{VLC_IP}/upload.json",
-                   headers={
-                       "Host": VLC_IP,
-                       "Connection": "keep-alive",
-                       "Content-Type": file.Monitor.content_type
-                    },
-                   data =file.Monitor
-                   )
-    except requests.exceptions.ConnectionError:
-        if isinstance(Log, Utils.Logging):
-            Log.Add(f"Error: Could not connect to {VLC_IP}.")
-        exit(1)
-            
-    if isinstance(Log, Utils.Logging):
-        Log.AddUploadResult(req.status_code,file.Name)
-    return 0
-def Main(VLC_IP: str,*MediaFiles: tuple[str]) -> None:
-    LoggingName = os.path.basename(__file__).split('.')[0]
-    Log = Utils.Logging(LoggingName,VLC_IP)
-    try:
-        MediaFiles: list[File.File] = File.ProcessMediaFileInput(Log,*MediaFiles)
+        LoggingName = Utils.GetFileName(__file__)
+        Log = Utils.Logging(LoggingName,VLC_IP)
+        MediaFiles: list[File.File] = File.ProcessMediaFileInput(Log,loadingbar,*MediaFiles)
         if MediaFiles == []:
-            Log.Add("Exiting program due to the lack of media files in the specified directory.")
-            exit(1)
+            Log.ExitingDueTo("the lack of media files in the specified directory")
         Log.Add("Starting Uploading phase.")
-        for Item in MediaFiles:
-            setup = Item.SetupUpload()
-            if setup[0]:
-                Upload(VLC_IP,Item,Log)
-            else:
-                Log.Add(setup[1])
+        Uploader.UploadAll(
+            VLC_IP = VLC_IP,
+            MediaFiles = MediaFiles,
+            Log = Log
+            )
         Log.Add("Finished Uploading all Media files.")
     except KeyboardInterrupt:
-        Log.Add("Exiting program due to keyboard interrupt.")
-        exit(1)
+        Log.ExitingDueTo("keyboard interrupt")
     except Exception as E:
-        Log.Add(str(E))
+        Log.ExitingError(str(E))
 
 if __name__ == "__main__":
     fire.Fire(Main)
